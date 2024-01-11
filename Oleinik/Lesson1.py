7213ac9d64a45b19b4ddb0afef60fbb8b27063fa
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-01-11 19:01:14 +0200

    Add files via upload
    Доделал дз

--- Oleinik/Lesson1.py
+++ Oleinik/Lesson1.py
@@ -23,13 +23,63 @@ print("Hello World!")
 # nimi = "Jaak"
 # pikkus = 16.5
 # kas_kaib_koolis = True
-#
 # print(f"Vanuse tuup: {type(vanus)}")
 # print(f"Nimi tuup: {type(nimi)}")
 # print(f"Pikkus tuup {type(pikkus)}")
 # print(f"Kas kaib koolis tuup {type(kas_kaib_koolis)}")
 
 
+         #4
+# print("mis teie puu ümbermõõd?")
+# c = float(input("puu ümbermõõd"))
+# pi = 3.14
+# diametr = c/pi
+# print(f" teie puu läbimõõdu on {diametr}")
+
+
+
+
          #3
+# kommid = 13
+# print(kommid)
+# while True:
+#     print("Mida te soovite?")
+#     print("mis sa hakkad tegima?\n1. votta natuke komme\n2.stop programm")
+#     a = int(input('tee'))
+#     if a == 1:
+#         print("mitu?")
+#         b = int(input("sinu arv"))
+#         print(kommid - b)
+    # if a == 2:
 
 
+    #5
+# print("kirjutage 2 numbri")
+# N = float(input("Kirjutage pikkus"))
+# M = float(input("Kirjutage laius"))
+# diagonaal = (N**2+M**2)
+# print(f"Teie diagonaal on {diagonaal}")
+
+
+     #7
+# A = float(input("Kirjutage esimene number"))
+# B = float(input("Kirjutage teine number"))
+# C = float(input("Kirjutage kolmas number"))
+# D = float(input("Kirjutage neljas number"))
+# E = float(input("Kirjutage viies number"))
+# keskmine = ((A+B+C+D+E)/5)
+# print(f"Teie aritmeetilise keskmise arvu on {keskmine}")
+
+     #8
+# print("  @..@")
+# print(" (----)")
+# print("( \__/ )")
+# print(" ^^ "" ^^ ")
+
+
+     #9
+# A = float(input("Kirjutage esimene külg"))
+# B = float(input("Kirjutage teine külg"))
+# C = float(input("Kirjutage kolmas külg)"))
+# ümbermõõt = (A+B+C)
+# print(f"ümbermõõt on {ümbermõõt}")
