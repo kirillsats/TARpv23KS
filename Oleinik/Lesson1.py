5643a4dc21610bd231671055ff3f8da94653c6a1
kirieshka v <kirieshka@MacBook-Air-kirieshka.local>
2024-01-11 12:15:17 +0200

    Initial commit

---
+++ Oleinik/Lesson1.py
@@ -0,0 +1,35 @@
+print("Hello World!")
+
+
+             #10
+# from random import *
+# P=randint(1,5)
+# hind=12.90
+# hind*=1.1 #hind koos jatatega
+# osa=round(hind/P,2)
+# print("Iga sõber maksab: ", osa)
+
+         #6
+# try:
+#     aeg = float(input("Mitu tundi kulus sõiduks? "))
+#     teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+#     kiirus = aeg / teepikkus
+#     print("Sinu kiirus oli " + str(kiirus) + " km/h")
+# except:
+#      print("Viga andmetüübiga")
+
+        #2
+# vanus = 18
+# nimi = "Jaak"
+# pikkus = 16.5
+# kas_kaib_koolis = True
+#
+# print(f"Vanuse tuup: {type(vanus)}")
+# print(f"Nimi tuup: {type(nimi)}")
+# print(f"Pikkus tuup {type(pikkus)}")
+# print(f"Kas kaib koolis tuup {type(kas_kaib_koolis)}")
+
+
+         #3
+
+
